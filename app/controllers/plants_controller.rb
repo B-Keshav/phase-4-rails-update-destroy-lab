88430a88b6402c74a3dbb04039e7b9bc21679f85
Keshav Bhati <keshavbhati2000@gmail.com>
2022-11-22 15:04:24 -0500

    commit tree 117669fceb9c96be8a26cecaff024e05d367976e

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -18,9 +19,30 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content 
+    # render json: plant.destroy
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found_response
+    render json: {error: "Plant Was Not Found"}, status: :not_found 
   end
 end
